2b5fd27bb9e7d929162e31cc98da5456788ee332
Trần Đức Huy <95224307+duchuys31@users.noreply.github.com>
2021-12-01 22:54:17 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std; 
+//S31[:v]
+#define int long long 
+#define fi first
+#define se second
+typedef pair <int,int> ii;
+typedef vector<int> v;
+const int oo=LONG_LONG_MAX;
+const int mod=1e9+7;
+const int N=1e5+5;
+int di[]={-1,0,0,1};
+int dj[]={0,-1,1,0};
+//
+
+//
+main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    // freopen("input.txt", "r", stdin);
+	// freopen("output.txt", "w", stdout);
+    return 0;
+}
+//
