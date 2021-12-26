dce875897fda2b46e0a6de289124855faf5a4048
Trần Đức Huy <95224307+duchuys31@users.noreply.github.com>
2021-12-26 15:30:47 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,22 +2,33 @@
 using namespace std; 
 //S31[:v]
 #define int long long 
+#define ll long long
+#define db double
 #define fi first
 #define se second
+
 typedef pair <int,int> ii;
 typedef vector<int> v;
-const int oo=LONG_LONG_MAX;
-const int mod=1e9+7;
-const int N=1e5+5;
-int di[]={-1,0,0,1};
-int dj[]={0,-1,1,0};
-//
 
+const ll oo=LONG_LONG_MAX;
+const int mod=1e9+7;
+const int N=1e6+5;
+
+int di[]={-1,0,0,1,-1,-1,1,1};
+int dj[]={0,-1,1,0,-1,1,-1,1};
+//
+void solution(){
+}
 //
 main(){
     ios_base::sync_with_stdio(0);cin.tie(0);
     // freopen("input.txt", "r", stdin);
 	// freopen("output.txt", "w", stdout);
+    int t=1;
+    // cin>>t;
+    while(t--){
+        solution();
+    }
     return 0;
 }
 //
