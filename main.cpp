21256bf9b5a81171a85f7f226f3fd42b330fcb84
Trần Đức Huy <95224307+duchuys31@users.noreply.github.com>
2022-04-20 17:42:03 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-#include<bits/stdc++.h>
-using namespace std; 
-/*Start*/
+#include <bits/stdc++.h>
+using namespace std;
+/* 𝑴𝒚 𝒄𝒂𝒕 𝒊𝒔 𝒍𝒐𝒐𝒌𝒊𝒏𝒈 𝒂𝒕 𝒚𝒐𝒖 ◉‿◉. */
+/* 𝑭𝒆𝒆𝒅 𝒊𝒕 𝒂𝒏𝒅 𝒊𝒕 𝒘𝒊𝒍𝒍 𝒉𝒆𝒍𝒑 𝒚𝒐𝒖 𝒖𝒏𝒅𝒆𝒓𝒔𝒕𝒂𝒏𝒅 𝒕𝒉𝒆 𝒑𝒓𝒐𝒃𝒍𝒆𝒎 (｡◕‿◕｡). */
 /*
    ██          ██
    █▒█        █▒█
@@ -8,7 +9,7 @@ using namespace std;
   █▒████████▒▒█
   █▒████▒▒█▒▒██
   ████▒▒▒▒▒████
-   █▒▒▒▒▒▒▒████
+   █▒▒▒▒▒▒▒███
   █▒▒▒▒▒▒▒▒████      █
  ██▒█▒▒▒▒▒█▒▒████  █▒█
  █▒█●█▒▒▒█●█▒▒███ █▒▒█
@@ -24,36 +25,57 @@ using namespace std;
      █▒████▒▒▒▒████
       █▒███▒██████
 */
-#define int long long 
+#define int long long
 #define ll long long
 #define db double
-#define fi first 
+#define fi first
 #define se second
+#define all(a) a.begin(), a.end()
+#define reset(a, val) memset(a, val, sizeof(a));
+#define For(i, a, b) for (int i = a; i <= b; i++)
+#define rFor(i, a, b) for (int i = a; i >= b; i--)
 
-typedef pair <int,int> ii;
+typedef pair<int, int> ii;
+typedef pair<db, db> dd;
 typedef vector<int> v;
 
-const ll oo=1e13;
-const int mod=1e9+7;
-const int N=1e6+5;
+const ll oo = 9223372036854775807;
+const int mod = 1e9 + 7;
+const int N = 1e6 + 5;
 
-int di[]={-1,0,0,1,-1,-1,1,1};
-int dj[]={0,-1,1,0,-1,1,-1,1};
+int di[] = {-1, 0, 0, 1, -1, -1, 1, 1};
+int dj[] = {0, -1, 1, 0, -1, 1, -1, 1};
 /**/
 /*Main*/
-void init(){}
-void solution(){}
+void init()
+{
+}
+void solution()
+{
+}
 /**/
-main(){
-    ios_base::sync_with_stdio(0);cin.tie(0);
+main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     // freopen("input.txt", "r", stdin);
-	// freopen("output.txt", "w", stdout);
-    int t=1;
-    // cin>>t;
-    while(t--){
+    // freopen("output.txt", "w", stdout);
+    int t = 1;
+    // cin >> t;
+    while (t--)
+    {
+        // db start = (db)(clock());
         init();
         solution();
+        // db end = (db)(clock());
+        // cout << (end - start) / CLOCKS_PER_SEC <<"s"<<endl;
     }
     return 0;
 }
-/*THE END*/
+/* 𝑴𝒚 𝒄𝒐𝒅𝒆 𝒉𝒂𝒔 𝒆𝒏𝒅𝒆𝒅. */
+/* 𝑺𝒆𝒆 𝒚𝒐𝒖 𝒂𝒈𝒂𝒊𝒏 ❤ */
+/*
+♫ ♪ ♫ ♪ ♫ ♪ ♫ ♪
+▄ █ ▄ █ ▄ ▄ █ ▄ █ ▄ █
+Min- - - - - - - - - - - -●Max
+*/
