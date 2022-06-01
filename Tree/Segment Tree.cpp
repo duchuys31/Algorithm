74ef7a099d7da5344d130e9741b4932b8cadfee0
Trần Đức Huy <95224307+duchuys31@users.noreply.github.com>
2022-06-02 04:29:45 +0700

    Update Segment Tree.cpp

--- Tree/Segment Tree.cpp
+++ Tree/Segment Tree.cpp
@@ -1,138 +1,59 @@
 struct Segment_Tree
 {
-    int Max[4 * N], Min[4 * N], Sum[4 * N];
-    int Tree[4 * N], lazy[4 * N];
-    void set_min()
+    int seg[4 * N], lazy[4 * N];
+
+    void down(int id)
     {
-        for (int i = 0; i <= 4 * N; i++)
-            Min[i] = oo;
+        int t = lazy[id];
+        seg[id << 1] += t;
+        seg[id << 1 | 1] += t;
+        lazy[id << 1] += t;
+        lazy[id << 1 | 1] += t;
+        lazy[id] = 0;
     }
-    void build(int a[], int node, int l, int r)
+
+    void update(int id, int l, int r, int u, int val)
+    {
+        if (r < u)
+            return;
+        if (l >= u)
+        {
+            seg[id] += val;
+            lazy[id] += val;
+            return;
+        }
+        down(id);
+        int mid = (l + r) >> 1;
+        update(id << 1, l, mid, u, val);
+        update(id << 1 | 1, mid + 1, r, u, val);
+        seg[id] = max(seg[id << 1], seg[id << 1 | 1]);
+    }
+
+    int get(int id, int l, int r, int u, int v)
+    {
+        if (l > v || r < u)
+            return -oo;
+        if (l >= u && r <= v)
+        {
+            return seg[id];
+        }
+        down(id);
+        int mid = (l + r) >> 1;
+        int v1 = get(id << 1, l, mid, u, v);
+        int v2 = get(id << 1 | 1, mid + 1, r, u, v);
+        return max(v1, v2);
+    }
+
+    void build(int a[], int id, int l, int r)
     {
         if (l == r)
         {
-            // Tree[node] = a[l];
-            // Max[node] = a[l];
-            // Min[node] = a[l];
-            // Sum[node] = a[l];
-        }
-        else
-        {
-            build(a, 2 * node, l, (l + r) / 2);
-            build(a, 2 * node + 1, (l + r) / 2 + 1, r);
-            // Tree[node] = (Tree[node * 2], Tree[node * 2 + 1]);
-            // Max[node] = max(Max[node * 2], Max[node * 2 + 1]);
-            // Min[node] = min(Min[node * 2], Min[node * 2 + 1]);
-            // Sum[node] = Sum[node * 2] + Sum[node * 2 + 1];
-        }
-    }
-    void update(int node, int l, int r, int left, int right, int value)
-    {
-        if (lazy[node] != 0)
-        {
-            // Tree[node] += lazy[node];
-            // Max[node] += lazy[node];
-            // Min[node] += lazy[node];
-            // Sum[node] += lazy[node];
-            if (l != r)
-            {
-                lazy[node * 2] += lazy[node];
-                lazy[node * 2 + 1] += lazy[node];
-            }
-            lazy[node] = 0;
-        }
-        if (r < left || l > right)
-            return;
-        if (l >= left && r <= right)
-        {
-            // Tree[node] += value;
-            // Max[node] += value;
-            // Min[node] += value;
-            // Sum[node] += value;
-            if (l != r)
-            {
-                lazy[node * 2] += value;
-                lazy[node * 2 + 1] += value;
-            }
+            seg[id] = a[l];
             return;
         }
-        update(node * 2, l, (l + r) / 2, left, right, value);
-        update(node * 2 + 1, (l + r) / 2 + 1, r, left, right, value);
-        // Tree[node] = (Tree[node * 2], Tree[node * 2 + 1]);
-        // Max[node] = max(Max[node * 2], Max[node * 2 + 1]);
-        // Min[node] = min(Min[node * 2], Min[node * 2 + 1]);
-        // Sum[node] = Sum[node * 2] + Sum[node * 2 + 1];
+        int mid = (l + r) >> 1;
+        build(a, id << 1, l, mid);
+        build(a, id << 1 | 1, mid + 1, r);
+        seg[id] = max(seg[id << 1], seg[id << 1 | 1]);
     }
-    int get(int node, int l, int r, int left, int right)
-    {
-        if (l > right || r < left)
-            return 0;
-        if (lazy[node] != 0)
-        {
-            Tree[node] += lazy[node];
-            if (l != r)
-            {
-                lazy[node * 2] += lazy[node];
-                lazy[node * 2 + 1] += lazy[node];
-            }
-            lazy[node] = 0;
-        }
-        if (l >= left && r <= right)
-            return Tree[node];
-        return (get(node * 2, l, (l + r) / 2, left, right), get(node * 2 + 1, (l + r) / 2 + 1, r, left, right));
-    }
-    int get_max(int node, int l, int r, int left, int right)
-    {
-        if (l > right || r < left)
-            return 0;
-        if (lazy[node] != 0)
-        {
-            Max[node] += lazy[node];
-            if (l != r)
-            {
-                lazy[node * 2] += lazy[node];
-                lazy[node * 2 + 1] += lazy[node];
-            }
-            lazy[node] = 0;
-        }
-        if (l >= left && r <= right)
-            return Max[node];
-        return max(get(node * 2, l, (l + r) / 2, left, right), get(node * 2 + 1, (l + r) / 2 + 1, r, left, right));
-    }
-    int get_min(int node, int l, int r, int left, int right)
-    {
-        if (l > right || r < left)
-            return oo;
-        if (lazy[node] != 0)
-        {
-            Min[node] += lazy[node];
-            if (l != r)
-            {
-                lazy[node * 2] += lazy[node];
-                lazy[node * 2 + 1] += lazy[node];
-            }
-            lazy[node] = 0;
-        }
-        if (l >= left && r <= right)
-            return Min[node];
-        return min(get(node * 2, l, (l + r) / 2, left, right), get(node * 2 + 1, (l + r) / 2 + 1, r, left, right));
-    }
-    int get_sum(int node, int l, int r, int left, int right)
-    {
-        if (l > right || r < left)
-            return 0;
-        if (lazy[node] != 0)
-        {
-            Sum[node] += lazy[node];
-            if (l != r)
-            {
-                lazy[node * 2] += lazy[node];
-                lazy[node * 2 + 1] += lazy[node];
-            }
-            Sum[node] = 0;
-        }
-        if (l >= left && r <= right)
-            return Sum[node];
-        return get(node * 2, l, (l + r) / 2, left, right)+ get(node * 2 + 1, (l + r) / 2 + 1, r, left, right);
-    }
-};
+}; 
