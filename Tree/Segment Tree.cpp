75a5871be99aad2344288d79ead04f4eb5f8f30d
Trần Đức Huy <95224307+duchuys31@users.noreply.github.com>
2022-07-08 15:06:05 +0700

    Update Segment Tree.cpp

--- Tree/Segment Tree.cpp
+++ Tree/Segment Tree.cpp
@@ -1,7 +1,15 @@
-struct Segment_Tree
+struct Seg
 {
     int seg[4 * N], lazy[4 * N];
-
+    void to_set(int x)
+    {
+        for (int i = 0; i <= 4 * N; i++)
+            seg[i] = x;
+    }
+    int merge(int x, int y)
+    {
+        return max(x, y);
+    }
     void down(int id)
     {
         int t = lazy[id];
@@ -11,7 +19,6 @@ struct Segment_Tree
         lazy[id << 1 | 1] += t;
         lazy[id] = 0;
     }
-
     void update(int id, int l, int r, int u, int val)
     {
         if (r < u)
@@ -26,7 +33,7 @@ struct Segment_Tree
         int mid = (l + r) >> 1;
         update(id << 1, l, mid, u, val);
         update(id << 1 | 1, mid + 1, r, u, val);
-        seg[id] = max(seg[id << 1], seg[id << 1 | 1]);
+        seg[id] = merge(seg[id << 1], seg[id << 1 | 1]);
     }
 
     int get(int id, int l, int r, int u, int v)
@@ -41,7 +48,7 @@ struct Segment_Tree
         int mid = (l + r) >> 1;
         int v1 = get(id << 1, l, mid, u, v);
         int v2 = get(id << 1 | 1, mid + 1, r, u, v);
-        return max(v1, v2);
+        return merge(v1, v2);
     }
 
     void build(int a[], int id, int l, int r)
@@ -54,6 +61,6 @@ struct Segment_Tree
         int mid = (l + r) >> 1;
         build(a, id << 1, l, mid);
         build(a, id << 1 | 1, mid + 1, r);
-        seg[id] = max(seg[id << 1], seg[id << 1 | 1]);
+        seg[id] = merge(seg[id << 1], seg[id << 1 | 1]);
     }
-}; 
+};
