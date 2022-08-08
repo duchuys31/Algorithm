3983ca054c1d2aafa0ff991b454bbe4fd8234c27
Trần Đức Huy <95224307+duchuys31@users.noreply.github.com>
2022-08-09 02:05:31 +0700

    Update nCk.cpp

--- nCk.cpp
+++ nCk.cpp
@@ -1,21 +1,14 @@
-int fac[N];
-int fac_inv[N];
-int inv[N];
+ll inv[N], fac[N], Inv[N];
+
+ll nCk(int n, int m) { return (m > n) ? 0 : ((fac[n] * Inv[n - m]) % mod) * Inv[m] % mod; }
+
 void setup()
 {
-    int n = 1e5;
-    fac[0] = inv[1] = fac_inv[0] = 1;
-    for (int i = 2; i <= n; i++)
-    {
-        inv[i] = mod - (mod / i) * inv[mod % i] % mod;
-    }
-    for (int i = 1; i <= n; i++)
+    inv[0] = fac[0] = Inv[0] = inv[1] = fac[1] = Inv[1] = 1;
+    For(i, 2, (int)(2e5))
     {
         fac[i] = fac[i - 1] * i % mod;
-        fac_inv[i] = fac_inv[i - 1] * inv[i] % mod;
+        inv[i] = mod - (mod / i) * inv[mod % i] % mod;
+        Inv[i] = Inv[i - 1] * inv[i] % mod;
     }
-}
-int nCk(int n, int k)
-{
-    return fac[n] * fac_inv[k] % mod * fac_inv[n - k] % mod;
 }
