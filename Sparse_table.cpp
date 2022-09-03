a1232cec64afef40e859a77fabc791eaefb1579b
Trần Đức Huy <95224307+duchuys31@users.noreply.github.com>
2022-09-04 00:40:55 +0700

    Update Sparse_table.cpp

--- Sparse_table.cpp
+++ Sparse_table.cpp
@@ -1,31 +1,38 @@
-int n;
-int a[N];
-int dp[N][30];
-int merge(int x, int y)
+template <typename type>
+struct SparseTable
 {
-    return max(x, y);
-    // return min(x, y);
-    // return __gcd(x, y);
-}
-int get(int l, int r)
-{
-    int m = log2(r - l + 1);
-    return merge(dp[l][m], dp[r - (1 << m) + 1][m]);
-}
-/*Main*/
-void init()
-{
-}
-void solution()
-{
-    cin >> n;
-    read(a, n);
-    For(i, 1, n) dp[i][0] = a[i];
-    for (int j = 1; (1 << j) <= n; j++)
+    int n;
+    vec<vec<type>> spt;
+    v log;
+    SparseTable(int a = 0) : n(a)
     {
-        for (int i = 1; i + (1 << j) - 1 <= n; i++)
+        log.assign(n + 1, 0);
+        log[1] = 0;
+        for (int i = 2; i <= n; i++)
         {
-            dp[i][j] = merge(dp[i][j - 1], dp[i + (1 << j - 1)][j - 1]);
+            log[i] = log[i >> 1] + 1;
+        }
+        spt.assign(n + 1, vec<type>(log[n] + 1));
+    }
+    inline type merge(int x, int y)
+    {
+        return min(x, y);
+    }
+    inline void build(int a[])
+    {
+        for (int i = 1; i <= n; i++)
+            spt[i][0] = a[i];
+        for (int j = 1; (1 << j) <= n; j++)
+        {
+            for (int i = 1; i + (1 << j) - 1 <= n; i++)
+            {
+                spt[i][j] = merge(spt[i][j - 1], spt[i + (1 << j - 1)][j - 1]);
+            }
         }
     }
-}
+    inline int get(int l, int r)
+    {
+        int len = log[r - l + 1];
+        return merge(spt[l][len], spt[r - (1 << len) + 1][len]);
+    }
+};
